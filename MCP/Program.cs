c8da97ae5febdde210c1bca3d28a20e137a3c985
Marcin Gołębiowski <xormus@gmail.com>
2025-05-25 17:49:18 +0200

    Update MCP to support aggregation, add #localChunksSearch

--- MCP/Program.cs
+++ MCP/Program.cs
@@ -44,7 +44,6 @@ public class RequestData
     [JsonPropertyName("query")]
     public string Query { get; set; }
 
-
     [JsonPropertyName("threshold")]
     public decimal Threshold { get; set; }
 
@@ -54,6 +53,24 @@ public class RequestData
     [JsonPropertyName("return_content")]
     public bool ReturContent { get; set; }
 }
+
+
+public class ChunkRequestData
+{
+    [JsonPropertyName("query")]
+    public string Query { get; set; }
+
+    [JsonPropertyName("threshold")]
+    public decimal Threshold { get; set; }
+
+    [JsonPropertyName("aggregateByDocument")]
+    public bool AggregateByDocument { get; set; }
+
+    [JsonPropertyName("max_results")]
+    public int MaxResults { get; set; }
+
+}
+
 public class DocumentResponseData
 {
     [JsonPropertyName("success")]
@@ -66,6 +83,9 @@ public class DocumentResponseItemData
 {
     [JsonPropertyName("id")]
     public string Id { get; set; }
+
+    [JsonPropertyName("source")]
+    public string Source { get; set; }
 
     [JsonPropertyName("document")]
     public string Document { get; set; }
@@ -87,12 +107,44 @@ public class ChunkResponseItemData
     [JsonPropertyName("id")]
     public string Id { get; set; }
 
+    [JsonPropertyName("source")]
+    public string Source { get; set; }
+
     [JsonPropertyName("chunk")]
     public string Document { get; set; }
+
+    [JsonPropertyName("metadata")]
+    public ChunkMetadata Metadata { get; set; }
 
     [JsonPropertyName("similarity")]
     public decimal Similarity { get; set; }
 }
+
+public class ChunkMetadata
+{
+    [JsonPropertyName("line_range")]
+    public string LineRange { get; set; }
+}
+
+public class ChunkAggregatedResponseData
+{
+    [JsonPropertyName("success")]
+    public bool Success { get; set; }
+
+    [JsonPropertyName("results")]
+    public ChunkAggregatedResponseItemData[] Results { get; set; }
+}
+
+public class ChunkAggregatedResponseItemData
+{
+    [JsonPropertyName("source")]
+    public string Source { get; set; }
+
+    [JsonPropertyName("chunks")]
+    public ChunkResponseItemData[] Chunks { get; set; }
+
+}
+
 
 internal static class HttpClientExt
 {
@@ -109,8 +161,13 @@ internal static class HttpClientExt
         var result = JsonSerializer.Deserialize<DocumentResponseData>(content);
         return result;
     }
-    public static async Task<ChunkResponseData> ReadJsonChunksAsync(this HttpClient client, RequestData request, string requestUri, CancellationToken cancellationToken)
+
+    public static async Task<ChunkResponseData> ReadJsonChunksAsync(this HttpClient client, ChunkRequestData request, string requestUri, CancellationToken cancellationToken)
     {
+        if (request.AggregateByDocument == true)
+        {
+            throw new Exception("AggregateByDocument must be false to use this endpoint. Use ReadJsonChunksAggregatedAsync instead.");
+        }
         string jsonPayload = JsonSerializer.Serialize(request);
         using var response = await client.PostAsync(requestUri, new StringContent(jsonPayload, System.Text.Encoding.UTF8, "application/json"));
         if (response.StatusCode != System.Net.HttpStatusCode.OK)
@@ -123,17 +180,44 @@ internal static class HttpClientExt
         return result;
     }
 
+    public static async Task<ChunkAggregatedResponseData> ReadJsonChunksAggregatedAsync(this HttpClient client, ChunkRequestData request, string requestUri, CancellationToken cancellationToken)
+    {
+        if (request.AggregateByDocument == false)
+        {
+            throw new Exception("AggregateByDocument must be true to use this endpoint. Use ReadJsonChunksAsync instead.");
+        }
+        string jsonPayload = JsonSerializer.Serialize(request);
+        using var response = await client.PostAsync(requestUri, new StringContent(jsonPayload, System.Text.Encoding.UTF8, "application/json"));
+        if (response.StatusCode != System.Net.HttpStatusCode.OK)
+        {
+            throw new HttpRequestException($"Request failed with status code {response.StatusCode}");
+        }
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync(cancellationToken);
+        var result = JsonSerializer.Deserialize<ChunkAggregatedResponseData>(content);
+        return result;
+    }
 }
 
 [McpServerToolType]
 public static class MCPs
 {
-    [McpServerTool(Name = "localDocumentsSearch"), Description("Finds the best local documents and chunks")]
+    [McpServerTool(Name = "localDocumentsSearch"), Description("Finds the best local documents")]
     public static string LocalDocumentsSearch(string prompt, HttpClient client, CancellationToken cancellationToken, [Description("Threashold of similarity of documents to return")] decimal threshold = 0.6m, int maxResults = 5, bool returnDocument = true)
     {   
         DocumentResponseData response = GetBestLocalDocument(prompt, client, threshold, maxResults, returnDocument, cancellationToken);
 
         return JsonSerializer.Serialize(response);
+    }
+
+
+    [McpServerTool(Name = "localChunksSearch"), Description("Finds the best local chunks")]
+    public static string LocalChunksSearch(string prompt, HttpClient client, CancellationToken cancellationToken, [Description("Threashold of similarity of chunks to return")] decimal threshold = 0.6m, int maxResults = 5, bool aggregateByDocument = false)
+    {
+        object response = GetBestLocalChunks(prompt, client, threshold, maxResults, aggregateByDocument, cancellationToken);
+
+        var json = JsonSerializer.Serialize(response);
+        return json;
     }
 
     private static DocumentResponseData GetBestLocalDocument(string prompt, HttpClient client, decimal threashold, int maxResults, bool returnDocument, CancellationToken cancellationToken)
@@ -159,13 +243,14 @@ public static class MCPs
         return response;
     }
 
-
-    private static ChunkResponseData GetBestLocalChunk(string prompt, HttpClient client, decimal threashold, CancellationToken cancellationToken)
+    private static object GetBestLocalChunks(string prompt, HttpClient client, decimal threashold, int maxResults, bool aggregateByDocument, CancellationToken cancellationToken)
     {
-        var requestData = new RequestData
+        var requestData = new ChunkRequestData
         {
             Query = prompt,
-            Threshold = threashold
+            Threshold = threashold,
+            AggregateByDocument = aggregateByDocument,
+            MaxResults = maxResults
         };
         var urlItem = Environment.GetEnvironmentVariable("Ollama-RAG-Sync-ProxyUrl");
 
@@ -174,9 +259,18 @@ public static class MCPs
         {
             url = urlItem;
         }
-        var task = client.ReadJsonChunksAsync(requestData, url, cancellationToken);
 
-        var response = task.Result;
-        return response;
+        if (requestData.AggregateByDocument)
+        {
+            var task = client.ReadJsonChunksAggregatedAsync(requestData, url, cancellationToken);
+            var response = task.Result;
+            return response;
+        }
+        else
+        {
+            var task = client.ReadJsonChunksAsync(requestData, url, cancellationToken);
+            var response = task.Result;
+            return response;
+        }
     }
 }
